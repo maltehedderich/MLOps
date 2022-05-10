278d4e0291c31807285c67e69e8412093bb0859e
Malte Hedderich <malte.hedderich@serviceware.de>
2022-05-10 11:12:34 +0200

    removed calories job

--- code/dagster-repo/dagster_repo/repo.py
+++ code/dagster-repo/dagster_repo/repo.py
@@ -1,8 +1,7 @@
 from dagster import repository
 from dagster_repo.example.wine_rfc.graph import wine_rfc_dev_job
-from dagster_repo.example.publish_to_triton.graph import publish_sklearn_job
+from dagster_repo.example.publish_bento.graph import publish_sklearn_job
 from dagster_repo.helper import create_buckets_dev_job
-from dagster_repo.presets import calories_test_job
 
 # start_repo_marker
 
@@ -10,7 +9,6 @@ from dagster_repo.presets import calories_test_job
 @repository
 def dev_repo():
     return [
-        calories_test_job,
         wine_rfc_dev_job,
         create_buckets_dev_job,
         publish_sklearn_job,
